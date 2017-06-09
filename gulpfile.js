ba3407bd00a53d365419c1ea508199187e74f334
Kevin Ball <kmball11@gmail.com>
2017-06-09 16:43:59 -0700

    Set up visual test flow

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var plugins = require('gulp-load-plugins');
 var autoprefixer = require('autoprefixer');
+var browser = require('browser-sync');
 
 var $ = plugins();
 
@@ -16,7 +17,31 @@ gulp.task('sass', function() {
     .pipe(gulp.dest('dist/css'));
 });
 
+// Watch files for changes
+gulp.task('watch', function() {
+  gulp.watch('scss/**/*', ['sass', browser.reload]);
+});
+
+
+
+gulp.task('test', ['sass', 'watch'], function() {
+  browser.init({
+    server: {
+      baseDir: 'test/visual',
+      directory: true,
+      routes: {
+        "/css": "dist/css"
+      }
+    }
+  });
+  gulp.watch(['test/visual/**/*'], ['test:reload']);
+});
+
+gulp.task('test:reload', function(done) {
+  browser.reload();
+  done();
+});
 
 gulp.task('build', ['sass']);
 
-gulp.task('default', ['build']);
+gulp.task('default', ['test']);
